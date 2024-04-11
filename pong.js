f65354de687a78d545f1551b8fd4010bbce35c00
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2024-04-11 14:13:18 +0900

    SDF

--- pong.js
+++ pong.js
@@ -7,6 +7,22 @@ const paddleInfo = {
     space: 10
 }
 
+const CONFLICT = {
+    NONE: 0,
+    HORIZONTAL: 1,
+    VERTICAL: 2
+};
+Object.freeze(CONFLICT);
+
+const GAME = {
+    LOSE_PLAYER1: 0,
+    LOSE_PLAYER2: 1,
+    LOSE_PLAYER3: 2,
+    LOSE_PLAYER4: 3,
+    CONTINUE: 4
+}
+Object.freeze(GAME);
+
 class AObject
 {
     constructor(posX, posY, diffX, diffY)
@@ -15,6 +31,14 @@ class AObject
         this._posY = posY;
         this._diffX = diffX;
         this._diffY = diffY;
+    }
+
+    reset(obj)
+    {
+        this._posX = obj._posX;
+        this._posY = obj._posY;
+        this._diffX = obj._diffX;
+        this._diffY = obj._diffY;
     }
 
     move() {}
@@ -28,6 +52,15 @@ class Paddle extends AObject
         super(posX, posY, diffX, diffY);
         this._widthX = widthX;
         this._widthY = widthY;
+        this._movingLeft = false;
+        this._movingRight = false;
+    }
+
+    reset(obj)
+    {
+        super(obj);
+        this._widthX = obj._widthX;
+        this._widthY = obj._widthY;
         this._movingLeft = false;
         this._movingRight = false;
     }
@@ -55,6 +88,17 @@ class Paddle extends AObject
             this._posY = afterY;
     }
 
+    inside(posX, posY)
+    {
+        if (posX > this._posX - this._widthX / 2 && posX < this._posX + this._widthX / 2
+            && posY > this._posY - this._widthY / 2 && posY < this._posY + this._widthY / 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     move()
     {
         if (!(this._movingLeft ^ this._movingRight))
@@ -80,26 +124,66 @@ class Ball extends AObject
         super(posX, posY, diffX, diffY);
         this._radius = radius;
     }
+    
+    reset(obj)
+    {
+        super(obj);
+        this._radius = obj._radius;
+    }
 
-    move()
+    /* X방향, Y방향 중 어느쪽에서 충돌했는지 알려주는 함수 */
+    _conflict(players)
+    {
+        let conflictResult = CONFLICT.NONE;
+
+        for (i = 0; i < players.length; ++i) {
+            if (players[i].inside(this._posX + this._radius, this._posY)
+                || players[i].inside(this._posX - this._radius, this._posY)) {
+                conflictResult |= CONFLICT.HORIZONTAL;
+            }
+            if (players[i].inside(this._posX, this._posY + this._radius)
+                || players[i].inside(this._posX, this._posY - this._radius)) {
+                conflictResult |= CONFLICT.VERTICAL;
+            }
+        }
+        return conflictResult;
+    }
+
+    move(players)
     {
         const afterX = this._posX + this._diffX;
         const afterY = this._posY + this._diffY;
+        const conflictResult = this._conflict(players);
 
-        if (afterX - this._radius >= 0 && afterX + this._radius <= cnvs.width
-            && afterY - this._radius >= 0 && afterY + this._radius <= cnvs.height) {
-            this._posX = afterX;
-            this._posY = afterY;
-            return;
-        }
-        if (afterX - this._radius < 0 || afterX + this._radius > cnvs.width) {
+        if (conflictResult & CONFLICT.HORIZONTAL) {
             this._diffX *= -1;
         }
-        if (afterY - this._radius < 0 || afterY + this._radius > cnvs.width) {
+        else if (afterX + this._radius > cnvs.width) {
+            this._diffX *= -1;
+            return LOSE.PLAYER1;
+        }
+        else if (afterX - this._radius < 0) {
+            this._diffX *= -1;
+            return LOSE.PLAYER2;
+        }
+        if (conflictResult & CONFLICT.VERTICAL) {
             this._diffY *= -1;
+        }
+        else if (afterY + this._radius > cnvs.width) {
+            this._diffY *= -1;
+            if (players.length >= 3) {
+                return LOSE.PLAYER3;
+            }
+        }
+        else if (afterY - this._radius < 0) {
+            this._diffY *= -1;
+            if (players.length >= 4) {
+                return LOSE.PLAYER4;
+            }
         }
         this._posX += this._diffX;
         this._posY += this._diffY;
+        return LOSE.CONTINUE;
     }
 
     draw()
@@ -110,28 +194,39 @@ class Ball extends AObject
     }
 };
 
+const initPaddles = [
+    new Paddle(paddleInfo.height, paddleInfo.width, paddleInfo.space + paddleInfo.height / 2, cnvs.height / 2, 0, 10),
+    new Paddle(paddleInfo.height, paddleInfo.width, cnvs.width - paddleInfo.space - paddleInfo.height / 2, cnvs.height / 2, 0, -10),
+    new Paddle(paddleInfo.width, paddleInfo.height, cnvs.width / 2, cnvs.height - paddleInfo.space - paddleInfo.height / 2, 10, 0),
+    new Paddle(paddleInfo.width, paddleInfo.height, cnvs.width / 2, paddleInfo.space + paddleInfo.height / 2, -10, 0)
+]
+
+const initBalls = [
+    new Ball(new Ball(cnvs.width / 2, cnvs.height / 2, 10, 10, 10))
+]
+
 class Player
 {
     constructor(playerNum)
     {
         switch (playerNum) {
         case 1:
-            this._paddle = new Paddle(paddleInfo.height, paddleInfo.width, paddleInfo.space + paddleInfo.height / 2, cnvs.height / 2, 0, 10);
+            this._paddle = new Paddle(initPaddles[0]);
             this._keydownHandler = this._makeKeyEventHandler(this._paddle, "w", "s", true);
             this._keyupHandler = this._makeKeyEventHandler(this._paddle, "w", "s", false);
             break;
         case 2:
-            this._paddle = new Paddle(paddleInfo.height, paddleInfo.width, cnvs.width - paddleInfo.space - paddleInfo.height / 2, cnvs.height / 2, 0, -10);
+            this._paddle = new Paddle(initPaddles[1]);
             this._keydownHandler = this._makeKeyEventHandler(this._paddle, "ArrowDown", "ArrowUp", true);
             this._keyupHandler = this._makeKeyEventHandler(this._paddle, "ArrowDown", "ArrowUp", false);
             break;
         case 3:
-            this._paddle = new Paddle(paddleInfo.width, paddleInfo.height, cnvs.width / 2, cnvs.height - paddleInfo.space - paddleInfo.height / 2, 10, 0);
+            this._paddle = new Paddle(initPaddles[2]);
             this._keydownHandler = this._makeKeyEventHandler(this._paddle, "c", "v", true);
             this._keyupHandler = this._makeKeyEventHandler(this._paddle, "c", "v", false);
             break;
         case 4:
-            this._paddle = new Paddle(paddleInfo.width, paddleInfo.height, cnvs.width / 2, paddleInfo.space + paddleInfo.height / 2, -10, 0);
+            this._paddle = new Paddle(initPaddles[3]);
             this._keydownHandler = this._makeKeyEventHandler(this._paddle, ".", ",", true);
             this._keyupHandler = this._makeKeyEventHandler(this._paddle, ".", ",", false);
             break;
@@ -164,13 +259,39 @@ class Player
     }
 };
 
+function scoreCalc(players, idx)
+{
+    for (i = 0; i < players.length; ++i) {
+        if (i != idx) {
+            ++players[i]._score;
+            console.log(`Player${i + 1}: ${players[i]._score}`);
+        }
+    }
+}
+
+function resetGame(players, balls)
+{
+    for (i = 0; i < players.length; ++i) {
+        players[i]._paddle.reset(initPaddles[i]);
+    }
+    for (i = 0; i < balls.length; ++i) {
+        balls[i].reset(initBalls[i]);
+    }
+}
+
+/* player가 움직인 후 그 다음에 공이 움직여야 공이 끼는 상태를 막을 수 있다. */
 function moveAll(players, balls)
 {
     players.forEach((instance) => {
         instance.move();
     })
     balls.forEach((instance) => {
-        instance.move();
+        const moveResult = instance.move(players);
+
+        if (moveResult !== GAME.NOCHANGE) {
+            scoreCalc(players, moveResult);
+            resetGame(players, balls);
+        }
     })
 }
 
